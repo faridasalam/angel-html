14b07efa1335727e6e3f459daf1f44b6e1f7c7c5
faridarmc <faridarmc@gmail.com>
2017-01-30 11:55:29 -0600

    updated

--- app/js/custom.js
+++ app/js/custom.js
@@ -12,8 +12,11 @@ jQuery(document).ready(function(){
           var heightParent = parseInt(elmnt.css('height').replace('px', '')) / 2;
           var widthParent = parseInt(elmnt.css('width').replace('px', '')) - 10;
           
-          if(!li.hasClass('open')) li.addClass('open')
-          else li.removeClass('open');
+          if(!li.hasClass('open')) {
+            li.addClass('open');
+          } else {
+            li.removeClass('open');
+          }
           $(this).next().css('top', heightParent + 'px');
           $(this).next().css('left', widthParent + 'px');
           
@@ -23,7 +26,7 @@ jQuery(document).ready(function(){
 
   //============================== ALL DROPDOWN ON HOVER =========================
   jQuery(document).ready(function(){
-    if($(".navbar").width() > 1007)
+    if($('.navbar').width() > 1007)
     {
       $('.nav .dropdown').hover(function() {
             $(this).addClass('open');
@@ -31,16 +34,16 @@ jQuery(document).ready(function(){
         function() {
             $(this).removeClass('open');
         });
-    };
+    }
   });
 
 //============================== BACK TO TOP =========================
   $(document).ready(function(){ 
     $(window).scroll(function(){ 
       if ($(this).scrollTop() > 10) { 
-        $('.backToTop').css("opacity", 1); 
+        $('.backToTop').css('opacity', 1); 
       } else { 
-        $('.backToTop').css("opacity", 0); 
+        $('.backToTop').css('opacity', 0); 
       } 
     }); 
   });
@@ -48,7 +51,7 @@ jQuery(document).ready(function(){
 //============================== BACK TO TOP SMOOTH SCROLL=========================
   $('.backToTop').on('click', function (event) {
     event.preventDefault();
-    $(document).off("scroll");
+    $(document).off('scroll');
 
     var target = this.hash,
     menu = target;
@@ -58,7 +61,7 @@ jQuery(document).ready(function(){
       'scrollTop': $target.offset().top+2
     }, 1500, 'swing', function () {
       window.location.hash = target;
-      $(document).on("scroll");
+      $(document).on('scroll');
     });
   });
 
@@ -82,7 +85,7 @@ jQuery(document).ready(function(){
         autoplay: autoplay,
         autoplayTimeout: interval,
         autoplayHoverPause: true,
-        smartSpeed:700,
+        smartSpeed:700
       });
     });
   } 
@@ -139,21 +142,12 @@ $(document).ready(function () {
 //============================== DATE-PICKER =========================
 $(document).ready(function () {
   $('.datepicker').datepicker({
-    startDate: "dateToday",
+    startDate: 'dateToday',
     autoclose: true
   });
 });
 
 //============================== SLIDER RESIZE =========================
-$(document).ready(function(){
-  resizeContent();
-
-  $(window).resize(function() {
-    resizeContent();
-  });
-});
-
-
 
 if ($(window).width() < 768) {
   function resizeContent() {
@@ -167,13 +161,22 @@ else {
     $('.slideResize').height($height);
   }
 }
+
+$(document).ready(function(){
+  resizeContent();
+
+  $(window).resize(function() {
+    resizeContent();
+  });
+});
+
 //============================== COUNT DOWN =========================
 $('#simple_timer').syotimer({
   year: 2018,
   month: 5,
   day: 9,
   hour: 20,
-  minute: 30,
+  minute: 30
 });
 
 //============================== SIDE TAB / NAV MENU TOGGLE =========================
@@ -192,35 +195,35 @@ $('.productSide li a').click(function() {
 var minimum = 12;
 var maximum = 300;
 
-$( "#price-range" ).slider({
+$( '#price-range' ).slider({
   range: true,
   min: minimum,
   max: maximum,
   values: [ minimum, maximum ],
   slide: function( event, ui ) {
-    $( "#price-amount-1" ).val( "$" + ui.values[ 0 ] );
-    $( "#price-amount-2" ).val( "$" + ui.values[ 1 ] );
+    $( '#price-amount-1' ).val( '$' + ui.values[ 0 ] );
+    $( '#price-amount-2' ).val( '$' + ui.values[ 1 ] );
   }
 });
 
-$( "#price-amount-1" ).val( "$" + $( "#price-range" ).slider( "values", 0 ));
-$( "#price-amount-2" ).val( "$" + $( "#price-range" ).slider( "values", 1 ));
+$( '#price-amount-1' ).val( '$' + $( '#price-range' ).slider( 'values', 0 ));
+$( '#price-amount-2' ).val( '$' + $( '#price-range' ).slider( 'values', 1 ));
 
 
 //============================== BOOTSTRA CAROUSEL SWIEP =========================
-$("#productSlider, #thubmnailTeamSlider").on("touchstart", function(event){
+$('#productSlider, #thubmnailTeamSlider').on('touchstart', function(event){
     var xClick = event.originalEvent.touches[0].pageX;
-    $(this).one("touchmove", function(event){
+    $(this).one('touchmove', function(event){
         var xMove = event.originalEvent.touches[0].pageX;
         if( Math.floor(xClick - xMove) < -5 ){
-            $("#productSlider, #thubmnailTeamSlider").carousel('prev');
+            $('#productSlider, #thubmnailTeamSlider').carousel('prev');
         }
         else if( Math.floor(xClick - xMove) > 5 ){
-            $("#productSlider, #thubmnailTeamSlider").carousel('next');
+            $('#productSlider, #thubmnailTeamSlider').carousel('next');
         }
     });
-    $(".carousel").on("touchend", function(){
-            $(this).off("touchmove");
+    $('.carousel').on('touchend', function(){
+            $(this).off('touchmove');
     });
 });
 
@@ -241,7 +244,7 @@ $("#productSlider, #thubmnailTeamSlider").on("touchstart", function(event){
             }
 
             itemToClone.children(':first-child').clone()
-            .addClass("cloneditem-"+(i))
+            .addClass('cloneditem-'+(i))
             .appendTo($(this));
           }
         });
@@ -250,7 +253,7 @@ $("#productSlider, #thubmnailTeamSlider").on("touchstart", function(event){
 //============================== FANCY BOX =========================
   $(document).ready(function() {
     
-    $("a.group").fancybox({
+    $('a.group').fancybox({
       'transitionIn'  : 'elastic',
       'transitionOut' : 'elastic',
       'speedIn'   : 600, 
@@ -263,8 +266,8 @@ $("#productSlider, #thubmnailTeamSlider").on("touchstart", function(event){
 //============================== SINGLE SERVICE LEFT TAB ========================= 
   $(document).ready(function($) {
     $('#singleServiceTab a').click(function (e) {
-      e.preventDefault()
-      $(this).tab('show')
+      e.preventDefault();
+      $(this).tab('show');
     });
 
     $('.nav-stacked li a').click(function() {
