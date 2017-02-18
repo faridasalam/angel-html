3421e0445a2060b29301858f167ac3e1c0259c8e
faridarmc <faridarmc@gmail.com>
2017-02-18 08:51:53 -0600

    17.02.2017

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,5 +1,39 @@
 jQuery(document).ready(function(){
   
+  //============================== MENU SCROLL =========================
+   $(window).load(function(){
+    $('.body-wrapper').each(function(){
+      var header_area = $('.header');
+      var main_area = header_area.children('.nav-wrapper');
+
+      var logo = main_area.find('.navbar-header');
+      var navigation = main_area.find('.navbar-collapse');        
+      var original = {
+        nav_top: navigation.css('margin-top')
+      };
+        
+      $(window).scroll(function(){
+        if( main_area.hasClass('bb-fixed-header') && ($(this).scrollTop() == 0 || $(this).width() < 750)){
+          main_area.removeClass('bb-fixed-header').appendTo(header_area);
+          navigation.animate({'margin-top': original.nav_top}, {duration: 300, queue: false, complete: function(){
+            header_area.css('height', 'auto');
+          }});                
+        }else if( !main_area.hasClass('bb-fixed-header') && $(this).width() > 750 &&
+          $(this).scrollTop() > header_area.offset().top + header_area.height() - parseInt($('html').css('margin-top')) ){
+          
+          header_area.css('height', header_area.height());
+          main_area.css({'opacity': '0'}).addClass('bb-fixed-header');
+          main_area.appendTo($('body')).animate({'opacity': 1});
+            
+          navigation.css({'margin-top': '0px'});
+        }       
+      });
+    });     
+    
+    $(window).trigger('resize');
+    $(window).trigger('scroll');
+  });
+
   //============================== SELECT BOX =========================
   $('.select-drop').selectbox();
 
